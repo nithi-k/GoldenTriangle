28d736a4fa6790a780c1a5bc40655fd0fb328539
Chavin2543 <rungallday@hotmail.com>
2022-02-14 21:10:59 +0700

    Attempt1

--- GoldenTriangle.playground/Contents.swift
+++ GoldenTriangle.playground/Contents.swift
@@ -1,10 +1,39 @@
 import UIKit
+import Foundation
 
 
 public func solution(_ T :[Int]) -> Bool {
-    //TODO: START IMPLEMENTING HERE
+    if T.count < 3 {
+        return false
+    }
+    
+    let sortedArray = T.sorted(by: {$0 < $1})
+    
+    for i in 0..<sortedArray.count-2 {
+        if sortedArray[i] + sortedArray[i+1] > sortedArray[i+2]{
+            return true
+        }
+    }
+    
     return false
+    
 }
 
-print(solution([10,50,5,1])) //MARK: TEST CASES
+print(solution(  [10,2,5,1,8,20])) //MARK: TEST CASES
+
+//1 2 5 8 10 20
+//
+//1 2 5
+//1 2 8
+//1 2 10
+//1 2 20
+//
+//2 5 8
+//2 5 10
+//2 5 20
+//
+//5 8 10
+//5 8 20
+//
+//1 8 20
 
